b73f55ea0f1024b2974e5b814a5d07ef04291c83
nftboi <ethnftboi@protonmail.com>
2022-07-31 12:32:13 -0700

    adding ws reaction to frontend

--- packages/frontend/src/components/Reactions/Reactions.tsx
+++ packages/frontend/src/components/Reactions/Reactions.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useRef, useState } from "react";
 import one from "reactions/src/images/1.png";
 import two from "reactions/src/images/2.png";
 import three from "reactions/src/images/3.png";
@@ -9,6 +9,7 @@ import reactionsJson from "reactions/src/reactions.json";
 import { useAppStore } from "../../stores";
 import { styles } from "./styles";
 import "./styles.css";
+import { socket } from "../../App";
 
 const images = [one, two, three, four, five, six];
 
@@ -20,6 +21,47 @@ type ReactionsProps = {
 export function Reactions(props: ReactionsProps) {
   const reactions = useAppStore((state) => state.reactions);
   const react = useAppStore((state) => state.react);
+  const refs = useRef<{ [key: string]: [string, HTMLDivElement] }>({});
+
+  const addFloaty = (element: HTMLDivElement, src: string) => {
+    const div = window.document.createElement("div");
+    const img = window.document.createElement("img");
+    const goUp = `goUp${Math.floor(Math.random() * 3)}`;
+    const leftRight = `leftRight${Math.floor(Math.random() * 3)}`;
+    div.classList.add("float-wrap");
+    div.classList.add(goUp);
+
+    div.append(img);
+    img.src = src;
+    img.classList.add("imageFloaty");
+    img.classList.add(leftRight);
+
+    div.addEventListener("animationend", () => {
+      div.remove();
+    });
+
+    element.append(div);
+  };
+
+  React.useEffect(() => {
+    const onReaction = ({
+      bidId,
+      reactionId,
+    }: {
+      bidId: string;
+      reactionId: string;
+    }) => {
+      if (bidId === props.bidId && refs.current[reactionId]) {
+        const [src, element] = refs.current[reactionId];
+        addFloaty(element, src);
+      }
+    };
+    socket.on("reaction", onReaction);
+
+    return () => {
+      socket.off("reaction", onReaction);
+    };
+  }, []);
 
   return (
     <div css={styles.wrap}>
@@ -27,26 +69,12 @@ export function Reactions(props: ReactionsProps) {
         const src = images[Number(reaction.id) - 1];
         return (
           <div
+            ref={(value: HTMLDivElement) => {
+              refs.current[reaction.id] = [src, value];
+            }}
             onClick={(el) => {
               react(props.bidId, reaction.id);
-              const div = window.document.createElement("div");
-              const img = window.document.createElement("img");
-              const goUp = `goUp${Math.floor(Math.random() * 3)}`;
-              const leftRight = `leftRight${Math.floor(Math.random() * 3)}`;
-              div.classList.add("float-wrap");
-              div.classList.add(goUp);
-
-              div.append(img);
-              img.src = src;
-              img.classList.add("imageFloaty");
-              img.classList.add(leftRight);
-
-              div.addEventListener("animationend", () => {
-                div.remove();
-              });
-
-              el.currentTarget.append(div);
-              console.log(el.currentTarget);
+              addFloaty(el.currentTarget, src);
             }}
             css={styles.imageWrap}
           >
